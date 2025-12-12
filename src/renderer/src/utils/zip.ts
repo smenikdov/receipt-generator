a2db5f6d4c51286ac2f522a9b43a1a24095e3b93
Stoic <sme.nik.dov@gmail.com>
2025-12-12 10:57:24 +0500

    perf(zip): сделал паралленльную генерацию blob

--- src/renderer/src/utils/zip.ts
+++ src/renderer/src/utils/zip.ts
@@ -5,13 +5,20 @@ import React from 'react';
 
 type PDFDocumentElement = React.ReactElement<DocumentProps>;
 
+const documentToBlob = async (docElement: React.ReactElement): Promise<Blob> => {
+    const instance = pdf();
+    instance.updateContainer(docElement);
+    const blob = await instance.toBlob();
+    return blob;
+};
+
 export const createZip = async (documents: { fileName: string; document: PDFDocumentElement }[]): Promise<Blob> => {
     const zip = new JSZip();
 
-    for (const { fileName, document } of documents) {
-        const blob = await pdf(document).toBlob();
+    await Promise.allSettled(documents.map(async ({ fileName, document }) => {
+        const blob = await documentToBlob(document);
         zip.file(fileName, blob);
-    }
+    }));
 
     const zipBlob = await zip.generateAsync({ type: 'blob' });
     return zipBlob;
